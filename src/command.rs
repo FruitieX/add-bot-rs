e01db5f50a0a0674b94f9b9c7d321a5d54a3cf6f
Rasmus Lövegren <fruitiex@gmail.com>
2021-12-04 18:36:22 +0200

    Support 3 digit add commands, support cmds ending with @botname

--- src/command.rs
+++ src/command.rs
@@ -45,12 +45,23 @@ pub fn parse_cmd(text: &str) -> Result<Option<Command>, Box<dyn std::error::Erro
             lazy_static! {
                 // Construct a regex that matches commands with four digits
                 // (such as /xxxx).
-                static ref RE: Regex = Regex::new(r"^/\d{4}$").unwrap();
+                static ref RE: Regex = Regex::new(r"^/(\d{3,4})(@(.+))?$").unwrap();
             }
 
-            if RE.is_match(text) {
-                let parsed_time = NaiveTime::parse_from_str(&text[1..], "%H%M")?;
-                Some(Command::Add(Some(parsed_time)))
+            let caps = RE.captures(text);
+
+            if let Some(caps) = caps {
+                let cmd = caps.get(1);
+                // let bot_name = caps.get(2);
+
+                if let Some(cmd) = cmd {
+                    let cmd = cmd.as_str();
+                    let cmd = format!("{:0>4}", cmd);
+                    let parsed_time = NaiveTime::parse_from_str(&cmd, "%H%M")?;
+                    Some(Command::Add(Some(parsed_time)))
+                } else {
+                    None
+                }
             } else {
                 None
             }
